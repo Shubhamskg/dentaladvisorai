64dbcc5e9b1555ff0b2feb771ff58d1fafb16541
SHUBHAM KUMAR <shubhamkumargupta786@gmail.com>
2024-02-17 15:55:29 +0530

    Update vite.config.js2

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     // port:3000,
     proxy: {
       "/api": {
-        target: https://dentaladvisor.onrender.com/,
+        target: "https://dentaladvisor.onrender.com/",
         changeOrigin: true,
         secure: true,
         

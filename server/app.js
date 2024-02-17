292c4dd0e41d29b27874edd95e874bb23b956754
Shubhamskg <shubhamkumargupta786@gmail.com>
2024-02-17 16:17:47 +0530

    dist2

--- server/app.js
+++ server/app.js
@@ -18,13 +18,13 @@ let port = process.env.PORT||5000
 // for production copy paste react js product build files in dist folder
 app.use(express.static('dist'))
 
-// app.use(cors({  origin: '*',
-// methods: ['GET','HEAD','PATCH', 'POST', 'PUT', 'DELETE'],
-// preflightContinue: false,
-// optionsSuccessStatus: 204,
-// allowedHeaders: ['Content-Type', 'Authorization'], 
-// credentials: true}))
-app.use(cors())
+app.use(cors({  origin: '*',
+methods: ['GET','HEAD','PATCH', 'POST', 'PUT', 'DELETE'],
+preflightContinue: false,
+optionsSuccessStatus: 204,
+allowedHeaders: ['Content-Type', 'Authorization'], 
+credentials: true}))
+// app.use(cors())
 app.use(cookieParser())
 // app.use(express.json())
 app.use(express.json({limit: '50mb'}));

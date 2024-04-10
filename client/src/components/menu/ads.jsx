697af53b9145d7f2e84def66086ccfbbbc28d98b
Shubhamskg <shubhamkumargupta786@gmail.com>
2024-04-10 10:07:37 +0530

    no news

--- client/src/components/menu/ads.jsx
+++ client/src/components/menu/ads.jsx
@@ -11,34 +11,34 @@ const Ads = ({ changeColorMode }) => {
     chatgpt : new URL('../../assets/img/chatgpt.png', import.meta.url).href,
     bardai : new URL('../../assets/img/bardai.png', import.meta.url).href,
   }
-  const [result,setResult]=useState({})
-  const GoogleSearch = async (term) => {
-    const { data } = await axios.get(
-      'https://www.googleapis.com/customsearch/v1',
-      {
-        params: {
-          key: import.meta.env.VITE_Google_Search_Api_Key,
-          cx: import.meta.env.VITE_Search_Engine_Id,
-          q: term,
-        },
-      }
-    );
-    return data;
-  };
- const [val,setVal]=useState("Todays dental News and Events")
- const setData = async (term) => {
-  const searches = await GoogleSearch(term);
-  setResult(searches);
-};
-useEffect(()=>{
-    setData(val)
-},[])
+//   const [result,setResult]=useState({})
+//   const GoogleSearch = async (term) => {
+//     const { data } = await axios.get(
+//       'https://www.googleapis.com/customsearch/v1',
+//       {
+//         params: {
+//           key: import.meta.env.VITE_Google_Search_Api_Key,
+//           cx: import.meta.env.VITE_Search_Engine_Id,
+//           q: term,
+//         },
+//       }
+//     );
+//     return data;
+//   };
+//  const [val,setVal]=useState("Todays dental News and Events")
+//  const setData = async (term) => {
+//   const searches = await GoogleSearch(term);
+//   setResult(searches);
+// };
+// useEffect(()=>{
+//     setData(val)
+// },[])
 
   return (
     <Fragment>
       <div className="container">
         <div className="box">
-              <div className="search">
+              {/* <div className="search">
                 <div className="btn">
               <input type='text' value={val} onChange={(e)=>setVal(e.target.value)}
               onKeyDown={(evt)=>{
@@ -83,8 +83,8 @@ useEffect(()=>{
             </div>
           ))}
               </div> 
-              </div>
-              {/* <NavLink to="https://www.google.com/" target="_blank" className="link">
+              </div> */}
+              <NavLink to="https://www.google.com/" target="_blank" className="link">
             <img src={IMAGES.google} className='ads-img' alt='first image' width={26} height={26}/>
               <div className="title">Google</div>
               </NavLink>
@@ -95,7 +95,7 @@ useEffect(()=>{
               <NavLink to="https://bard.google.com/" target="_blank" className="link">
             <img src={IMAGES.bardai} className='ads-img' alt='first image' width={26} height={26}/>
               <div className="title">BardAI</div>
-              </NavLink> */}
+              </NavLink>
             
             
         </div>

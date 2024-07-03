2d940993c12bf0713c8d97b11a671270116f75f3
Shubhamskg <shubhamkumargupta786@gmail.com>
2024-07-03 15:25:14 +0530

    reg

--- server/routes/feedback.js
+++ server/routes/feedback.js
@@ -6,6 +6,7 @@ import path from "path";
 let router = Router()
 router.post('/submit', async (req, res) => {
     // var pathname = url.parse(request.url).pathname;
+    console.log("body",req.body)
     const feedback_data = req.query
     console.log("req",req)
     console.log("query",req.query)
